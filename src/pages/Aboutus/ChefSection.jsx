f332cdeba6690cb058de464fd9992e4d01e7ab19
MdDilshad-dev <dilshadvns17@gmail.com>
2024-09-29 13:26:48 +0530

    import is missing

--- src/pages/Aboutus/ChefSection.jsx
+++ src/pages/Aboutus/ChefSection.jsx
@@ -1,6 +1,7 @@
 import Heading from './../../components/Heading';
 import React from 'react'
 import Img from "./../../components/Img";
+import Text from '../../components/Text';
 
 const ChefSection = () => {
   return (
