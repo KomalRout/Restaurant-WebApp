f0008c0212b4dc9fb4409a55e5d2d9e886f73ba0
MdDilshad-dev <dilshadvns17@gmail.com>
2024-09-29 20:26:36 +0530

    added screens in tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {md: { max: "1050px"}, sm: { max: "550px" }}
   },
   darkMode:'class',
   plugins: [],
